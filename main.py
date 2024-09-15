4145b51b005540d831ebd2da3748a0f348a7f33d
Tim <rrrraaaaaap@gmail.com>
2024-09-15 22:59:41 +0300

    add new code options

--- main.py
+++ main.py
@@ -18,8 +18,24 @@ target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+score_area_width = 200
+score_area_height = 50
 
-color = (random.randint(0, 255 ), random.randint(0, 255), random.randint(0, 255))
+hit_count = 0
+
+color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
+font = pygame.font.SysFont('Arial', 36)
+
+
+def get_new_target_position():
+    while True:
+        target_x = random.randint(0, SCREEN_WIDTH - target_width)
+        target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+        if not (target_x > SCREEN_WIDTH - score_area_width - target_width and target_y < score_area_height):
+            return target_x, target_y
+
+target_x, target_y = get_new_target_position()
+
 running = True
 while running:
     screen.fill(color)
@@ -29,10 +45,17 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                target_x, target_y = get_new_target_position()
+                hit_count += 1
+                print(f"Попаданий: {hit_count}")
+                color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
+
 
     screen.blit(target_img, (target_x, target_y))
+
+    hit_text = font.render(f"Попаданий: {hit_count}", True, (255, 255, 255))
+    text_width = hit_text.get_width()
+    screen.blit(hit_text, (SCREEN_WIDTH - text_width - 10, 10))
     pygame.display.update()
 
 pygame.quit()
